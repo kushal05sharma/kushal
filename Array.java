1ea8d5eecb9535e50cb9efc85586f87ce1073029
kushal05sharma <133697198+kushal05sharma@users.noreply.github.com>
2023-05-15 23:56:08 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String args []){
+        int marks [] = new int [4];
+        marks[0]=1;
+        marks[1]=2;
+        marks[2]=1;
+        marks[3]=1;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        //Printing Through loop 
+        for(int i =0 ; i<4; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
